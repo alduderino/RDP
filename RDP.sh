efa81c961bd806d5fd5fd03955caee23eaf706eb
Al U <alfonsou@defiant.madcyb.org>
2021-02-09 20:50:13 +0000

    2nd revision. Fixed the username

--- RDP.sh
+++ RDP.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 printf "Installing RDP Thanks for your Patience... " >&2
 {
-sudo useradd -m AL
-sudo adduser AL sudo
-echo 'AL:224488' | sudo chpasswd
+sudo useradd -m al
+sudo adduser al sudo
+echo 'al:224488' | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo apt-get update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
@@ -18,13 +18,13 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 sudo apt install nautilus nano -y 
-sudo adduser AL chrome-remote-desktop
+sudo adduser al chrome-remote-desktop
 } &> /dev/null &&
 printf "\nSetup Complete " >&2 ||
 printf "\nError Occured " >&2
 printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
 read -p "Paste Here: " CRP
-su - AL -c """$CRP"""
+su - al -c """$CRP"""
 printf 'Check https://remotedesktop.google.com/access/ \n\n'
 if sudo apt-get upgrade &> /dev/null
 then
